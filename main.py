0ff89d57fc969b475cb7dfe62b3ad9446f04383b
David Ang <cdang1024@gmail.com>
2021-11-18 17:32:16 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import tensorflow as tf
-from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
 
 import numpy as np
 import os
 import time
 
-path_to_file = tf.keras.utils.get_file("kanye_verses.txt")
+# creates array of each line as a separate sentence
+with open('kanye_verses.txt') as f:
+    lines = f.readlines()
+
+# creates dictionary of words assigned to tokens (enumerate each word for data manipulation)
+tokenizer = Tokenizer(num_words=100)
+tokenizer.fit_on_texts(lines)
+word_index = tokenizer.word_index
+print(word_index)
